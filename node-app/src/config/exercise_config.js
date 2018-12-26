6871804dc0d0347ec3c2c1879f9ae98996fcd3fd
Ashish Baghudana <ashish.baghudana26@gmail.com>
2018-12-27 00:05:06 +0530

    Read in semester config

--- node-app/src/config/exercise_config.js
+++ node-app/src/config/exercise_config.js
@@ -1,10 +1,9 @@
-import exercises_html from '../exercises/exercises.html'
 import ex1_html from '../exercises/exercise1.html'
 import ex2_html from '../exercises/exercise2.html'
 import ex3_html from '../exercises/exercise3.html'
 import ex4_html from '../exercises/exercise4.html'
-
-
+import exercises_html from '../exercises/exercises.html'
+import semester_based_config from "/home/courses/cs3214/admin/submissions/configurationFiles/spring2019.json";
 
 let exercise_config = {
     nav_targets: {
@@ -14,10 +13,10 @@ let exercise_config = {
                 label: "Exercises",
                 entries: [
                     {path: '/exercises', label: "Exercise Due Dates", templateUrl: exercises_html},
-                    {path: '/ex1', label: "Exercise 1", templateUrl: ex1_html, needauthentication: true, disabled: true},
-                    {path: '/ex2', label: "Exercise 2", templateUrl: ex2_html, disabled: true},
-                    {path: '/ex3', label: "Exercise 3", templateUrl: ex3_html, needauthentication: true, disabled: true},
-                    {path: '/ex4', label: "Exercise 4", templateUrl: ex4_html, disabled: true},
+                    {path: '/ex1', label: "Exercise 1", templateUrl: ex1_html, needauthentication: true, disabled: !semester_based_config.projecttargets.ex1.enabled},
+                    {path: '/ex2', label: "Exercise 2", templateUrl: ex2_html, disabled: !semester_based_config.projecttargets.ex2.enabled},
+                    {path: '/ex3', label: "Exercise 3", templateUrl: ex3_html, needauthentication: true, disabled: !semester_based_config.projecttargets.ex3.enabled},
+                    {path: '/ex4', label: "Exercise 4", templateUrl: ex4_html, disabled: !semester_based_config.projecttargets.ex4.enabled},
                 ]
             },
         ]
--- node-app/src/config/project_config.js
+++ node-app/src/config/project_config.js
@@ -3,11 +3,13 @@ This file maintains the configuration for the project target dropdowns
 and should be added to for changes to what projects are displayed
  */
 
-import projectspartial from '../projects/projects.html'
 import project1partial from '../projects/project1.html';
 import project2partial from '../projects/project2.html';
 import project3partial from '../projects/project3.html';
 import project4partial from '../projects/project4.html';
+import projectspartial from '../projects/projects.html';
+import semester_based_config from "/home/courses/cs3214/admin/submissions/configurationFiles/spring2019.json";
+
 let projectConfig = {
     nav_targets: {
         topbar: [],
@@ -15,11 +17,11 @@ let projectConfig = {
             {
                 label : "Projects",
                 entries: [
-                    { path: '/projects', label: "Project Due Dates", templateUrl: projectspartial, disabled: true },
-                    { path: '/project1', label: "Project 1", templateUrl: project1partial, disabled: true },
-                    { path: '/project2', label: "Project 2", templateUrl: project2partial, disabled: true },
-                    { path: '/project3', label: "Project 3", templateUrl: project3partial, disabled: true },
-                    { path: '/project4', label: "Project 4", templateUrl: project4partial, disabled: true },
+                    { path: '/projects', label: "Project Due Dates", templateUrl: projectspartial },
+                    { path: '/project1', label: "Project 1", templateUrl: project1partial, disabled: !semester_based_config.projecttargets.p1.enabled },
+                    { path: '/project2', label: "Project 2", templateUrl: project2partial, disabled: !semester_based_config.projecttargets.p2.enabled },
+                    { path: '/project3', label: "Project 3", templateUrl: project3partial, disabled: !semester_based_config.projecttargets.p3.enabled },
+                    { path: '/project4', label: "Project 4", templateUrl: project4partial, disabled: !semester_based_config.projecttargets.p4.enabled },
 
                 ]
             },
